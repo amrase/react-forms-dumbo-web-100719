ef4788d5456025600e54bf65edf3929347d24077
Amra Sezairi <amra.sezairi@hotmail.com>
2020-01-07 20:44:59 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,26 +3,41 @@ import React from 'react';
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: []
   }
 
-  handleFirstNameChange = event => {
+  handleFirstNameChange = event =>{
     this.setState({
       firstName: event.target.value
     })
   }
 
-  handleLastNameChange = event => {
+  handleLastNameChange = event =>{
     this.setState({
       lastName: event.target.value
     })
   }
+  handleSubmit = event =>{
+    event.preventDefault()
+    let formData = {firstName:this.state.firstName,lastName: this.state.lastName}
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+    })
+  }
 
   render() {
+    console.log(this.state.submittedData)
+    console.log(this.handleFirstNameChange)
     return (
-      <form>
+      <form onSubmit={event =>this.handleSubmit(event)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      <input type='submit'/>
       </form>
     )
   }
